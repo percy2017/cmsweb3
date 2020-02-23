23cddc98149a265f20d0f5c510e333fc74c84d1a
percy alvarez <percy.alvarez.2017@gmail.com>
2020-02-23 18:42:08 -0400

    commits 1

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'avatar' => null,
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'name' => 'editor',
+            'email' => 'luis.flores@gmail.com',
+            'avatar' => null,
+            'password' => Hash::make('password')
+        ]);
+    }
+}
