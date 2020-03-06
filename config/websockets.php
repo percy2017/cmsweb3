ff4160a5d0db414ec747d16bb0c151fbd43bd311
percy alvarez <percy.alvarez.2017@gmail.com>
2020-03-06 00:38:56 -0400

    commit 23

--- config/websockets.php
+++ config/websockets.php
@@ -110,13 +110,15 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        // 'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => '/etc/letsencrypt/live/loginweb.dev/cert.pem',
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        // 'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => '/etc/letsencrypt/live/loginweb.dev/privkey.pem',
 
         /*
          * Passphrase for your local_cert file.
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,8 +37,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    // encrypted: true
+    encrypted: true,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    // disableStats: true,
+    disableStats: true,
 });
